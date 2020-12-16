b34bdc58c368da029cda3e388481f595c3f951ab
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-12-16 16:05:28 +0100

    refactor(yatzy): chance function add *

--- python/yatzy.py
+++ python/yatzy.py
@@ -1,14 +1,11 @@
 class Yatzy:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(*dices): # refactorizado, permite introducir multiples valores a la función, no esta definida la cantidad de valores de entrada.
+        score = 0
+        for dice in dices:
+            score += dice
+        return score
 
     @staticmethod
     def yatzy(dice):
