f7cbda6dca92eb5fdffe1cca4bc50b16fa32c829
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-12-16 20:46:14 +0100

    add some tests

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -23,17 +23,19 @@ def test_ones(): # change name "1s" to "ones"
         assert 2 == Yatzy.ones(1,2,1,4,5)
         assert 0 == Yatzy.ones(6,2,2,4,5)
         assert 4 == Yatzy.ones(1,2,1,1,1)
+        assert 5 == Yatzy.ones(1,1,1,1,1) # added.
 
 
 def test_twos(): # change name "2s" to "twos"
         assert 4 == Yatzy.twos(1,2,3,2,6)
         assert 10 == Yatzy.twos(2,2,2,2,2)
+        assert 6 == Yatzy.twos(2,2,2,4,5) # added.
 
 
 def test_threes():
         assert 6 == Yatzy.threes(1,2,3,2,3)
         assert 12 == Yatzy.threes(2,3,3,3,3)
-
+        assert 9 == Yatzy.threes(3,3,3,2,1) # added.
 
 def test_fours_test():
         assert 12 == Yatzy(4,4,4,5,5).fours()
@@ -59,7 +61,7 @@ def test_one_pair():
         assert 12 == Yatzy.score_pair(5,3,6,6,5)
 
 
-def test_two_Pair():
+def test_two_pair():
         assert 16 == Yatzy.two_pair(3,3,5,4,5)
         assert 18 == Yatzy.two_pair(3,3,6,6,6)
         assert 0 == Yatzy.two_pair(3,3,6,5,4)
@@ -71,26 +73,26 @@ def test_three_of_a_kind():
         assert 9 == Yatzy.three_of_a_kind(3,3,3,3,5)
 
 
-def test_four_of_a_knd():
+def test_four_of_a_kind():
         assert 12 == Yatzy.four_of_a_kind(3,3,3,3,5)
         assert 20 == Yatzy.four_of_a_kind(5,5,5,4,5)
         assert 12 == Yatzy.four_of_a_kind(3,3,3,3,3)
         assert 0  == Yatzy.four_of_a_kind(3,3,3,2,1)
 
 
-def test_smallStraight():
+def test_small_straight():
         assert 15 == Yatzy.smallStraight(1,2,3,4,5)
         assert 15 == Yatzy.smallStraight(2,3,4,5,1)
         assert 0 == Yatzy.smallStraight(1,2,2,4,5)
 
 
-def test_largeStraight():
+def test_large_straight():
         assert 20 == Yatzy.largeStraight(6,2,3,4,5)
         assert 20 == Yatzy.largeStraight(2,3,4,5,6)
         assert 0 == Yatzy.largeStraight(1,2,2,4,5)
 
 
-def test_fullHouse():
+def test_full_house():
         assert 18 == Yatzy.fullHouse(6,2,2,2,6)
         assert 0 == Yatzy.fullHouse(2,3,4,5,6)
 
